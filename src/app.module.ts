ddfbb068e3d1fc4ba8c3259935009da2b3e986e0
angieene <angela.nerukh@gmail.com>
2023-05-03 17:15:32 +0300

    deleted the rest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { UrlModule } from "./url/url.module";
 import { configService } from "./config/config.service";
 
 @Module({
   imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()), UrlModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
